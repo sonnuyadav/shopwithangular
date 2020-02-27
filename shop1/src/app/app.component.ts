9b810ca8b12490cce6ade1eddd0b05ea08ed3557
sonu yadav <sonu@pokermagnet>
2020-02-27 12:29:54 +0530

    Added admore | remove

--- shop1/src/app/app.component.ts
+++ shop1/src/app/app.component.ts
@@ -7,4 +7,22 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'shop1';
+
+  viewMode ='map';
+
+  courses =[
+    {id:1,name:"BCA"},
+    {id:2,name:"MCA"},
+    {id:3,name:"MBA"},
+
+  ];
+  onClickAdd(){
+    this.courses.push({id:4,name:"BBA"});
+  };
+  onRemove(course){
+    
+    let index = this.courses.indexOf(course);
+    console.log("Course",course, "Index",index);
+    this.courses.splice(index,1);
+  };
 }
